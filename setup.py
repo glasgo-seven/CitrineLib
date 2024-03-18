7389119d529500dea90621349618e8ee146a9a32
glasgo-seven <glasgoseven@gmail.com>
2024-03-18 14:28:56 +0300

    preparing for pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name='Citrine',
+	version='0.0.1',
+	description='A library of scripts and algorithms for personal use',
+	
+	url='https://github.com/glasgo-seven/Citrine',
+
+	author='glasgo-seven',
+	author_email='glasgoseven@gmail.com',
+
+	packages=['Citrine'],
+	install_requires=[
+
+ 	],
+)
